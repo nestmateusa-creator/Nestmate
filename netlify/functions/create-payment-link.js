123cc5239fcbb89002222e537baad37f41cadbec
Charles <nestmateusa@gmail.com>
2025-09-07 09:27:00 -0400

    Add debugging to see which API key is being used

--- netlify/functions/create-payment-link.js
+++ netlify/functions/create-payment-link.js
@@ -1,4 +1,6 @@
-const stripe = require('stripe')('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');
+const API_KEY = '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';
+console.log('Using API Key:', API_KEY.substring(0, 20) + '...' + API_KEY.substring(API_KEY.length - 10));
+const stripe = require('stripe')(API_KEY);
 
 exports.handler = async (event, context) => {
   // Handle CORS preflight requests
